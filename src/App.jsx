c95a59cab21103cc42f7bed2f9e2c53435cc6ebc
naitik-dubey <naitikdubey2132006@gmail.com>
2025-07-29 12:43:14 +0530

    App jsx React Name commited

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>React + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -25,7 +25,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the React and React logos to learn more
       </p>
     </>
   )
