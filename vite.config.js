1a761a2644930c18d1c345c412353463807e47b6
naitik-dubey <naitikdubey2132006@gmail.com>
2025-07-29 12:20:48 +0530

    Vite Config Commited

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/reactcics/",
+  base: "/reactcicd/",
   plugins: [react()],
 })
